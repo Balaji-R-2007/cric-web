160c6eeb49e0bc6e071238d70cbe9f5cfdc17586
Balaji-R-2007 <balaji648balaji@gmail.com>
2024-10-08 14:20:24 +0530

    done

---
+++ src/app.js
@@ -0,0 +1,105 @@
+let sehwagimg=document.getElementById("sehwag-img")
+let sehwag=document.getElementById("sehwag")
+// add event lsitener that when the name is cliked, displays a popup about the information of the player. The display style should be block and use tranform with values 5em,-20em. When the second click is made the display can be made none to ensure on mouse leave image vanishes
+sehwag.addEventListener("click",function(){
+sehwagimg.style.display= "block";
+sehwagimg.style.transform= "translate(5em, -20em)";
+
+})
+sehwagimg.addEventListener("mouseleave",function(){
+
+sehwagimg.style.display="none";
+
+})
+
+  
+
+
+
+
+
+let sachinimg=document.getElementById("sachin-img")
+let sachin=document.getElementById("sachin")
+// add event lsitener that when the name is cliked, displays a popup about the information of the player. The display style should be block and use tranform with values 5em,-18em. When the second click is made the display can be made none to ensure on mouse leave image vanishes
+sachin.addEventListener("click",function(){
+  sachinimg.style.display= "block";
+  sachinimg.style.transform= "translate(5em, -18em)";
+  
+  })
+  sachinimg.addEventListener("mouseleave",function(){
+
+  sachinimg.style.display="none";
+    
+    })
+    
+
+let gautamimg=document.getElementById("gambhir-img")
+let gautam=document.getElementById("gautam")
+
+// add event lsitener that when the name is cliked, displays a popup about the information of the player. The display style should be block and use tranform with values 5em,-16em. When the second click is made the display can be made none to ensure on mouse leave image vanishes
+
+gautam.addEventListener("click",function(){
+  gautamimg.style.display= "block";
+  gautamimg.style.transform= "translate(5em, -16em)";
+  
+  })
+  gautamimg.addEventListener("mouseleave",function(){
+
+    gautamimg.style.display="none";
+      
+      })
+    
+let viratimg=document.getElementById("virat-img")
+let virat=document.getElementById("virat")
+// add event lsitener that when the name is cliked, displays a popup about the information of the player. The display style should be block and use tranform with values 5em,-14em. When the second click is made the display can be made none to ensure on mouse leave image vanishes
+virat.addEventListener("click",function(){
+  viratimg.style.display= "block";
+  viratimg.style.transform= "translate(5em, -14em)";
+  
+  })
+  viratimg.addEventListener("mouseleave",function(){
+
+    viratimg.style.display="none";
+      
+      })
+    
+
+let dhoniImg=document.getElementById("dhoni-img")
+let dhoni=document.getElementById("dhoni")
+// add event lsitener that when the name is cliked, displays a popup about the information of the player. The display style should be block and use tranform with values 5em,-12em. When the second click is made the display can be made none to ensure on mouse leave image vanishes
+dhoni.addEventListener("click",function(){
+  dhoniImg.style.display= "block";
+  dhoniImg.style.transform= "translate(5em, -12em)";
+  
+  })
+ dhoniImg.addEventListener("mouseleave",function(){
+
+    dhoniImg.style.display="none";
+      
+      })
+
+
+// Additional Challenge
+// If you want the data to get populated for all players, pls feel free and try the same for other players as well :)
+let yuvrajimg=document.getElementById("yuvraj-img")
+let yuvraj=document.getElementById("yuvraj")
+
+
+let sureshimg=document.getElementById("raina-img")
+let suresh=document.getElementById("raina")
+
+
+let singhimg=document.getElementById("bhajan-img")
+let singh=document.getElementById("singh")
+
+
+let khanimg=document.getElementById("khan-img")
+let khan=document.getElementById("khan")
+
+
+let patelimg=document.getElementById("patel-img")
+let patel=document.getElementById("patel")
+
+
+let sreeshantimg=document.getElementById("shanth-img")
+let sreeshant=document.getElementById("shanth")
